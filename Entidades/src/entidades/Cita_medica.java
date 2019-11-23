7c69b7003160c58dafd387672aef5e06737aebee
jorgeLopez98 <56857024+jorgeLopez98@users.noreply.github.com>
2019-11-23 18:04:08 -0500

    jorge_entidades

---
+++ Entidades/src/entidades/Cita_medica.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+/**
+ *
+ * @author Jorge Lopez
+ */
+public class Cita_medica {
+    private int id_cita;
+    private String fecha ;
+    private int numero_cita;
+    private boolean estado;
+    private int id_medico;
+    private int id_especialidad;
+
+    public int getId_cita() {
+        return id_cita;
+    }
+
+    public void setId_cita(int id_cita) {
+        this.id_cita = id_cita;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getNumero_cita() {
+        return numero_cita;
+    }
+
+    public void setNumero_cita(int numero_cita) {
+        this.numero_cita = numero_cita;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public int getId_medico() {
+        return id_medico;
+    }
+
+    public void setId_medico(int id_medico) {
+        this.id_medico = id_medico;
+    }
+
+    public int getId_especialidad() {
+        return id_especialidad;
+    }
+
+    public void setId_especialidad(int id_especialidad) {
+        this.id_especialidad = id_especialidad;
+    }
+    
+    
+    
+} 
