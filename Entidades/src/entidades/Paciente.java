a92076e08146b6f3cf4331c3e47e7036113ce3fa
ricky3498 <55850930+ricky3498@users.noreply.github.com>
2019-11-23 17:05:18 -0500

    entidades

---
+++ Entidades/src/entidades/Paciente.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+/**
+ *
+ * @author jhon
+ */
+public class Paciente {
+    private int id_pac;
+    private String dni_pac;
+    private String nombre_pac;
+    private String apellido_pac;
+    private int id_usuario;
+    
+}
