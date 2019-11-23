50336b44f98ab4e96cf7c918e7101b1ebef594dc
Alberto1004 <56856573+Alberto1004@users.noreply.github.com>
2019-11-23 18:06:29 -0500

    delete

--- Entidades/src/entidades/Detalle_atencion.java
+++ Entidades/src/entidades/Detalle_atencion.java
@@ -10,5 +10,50 @@ package entidades;
  * @author Alberto
  */
 public class Detalle_atencion {
+    private int id_detalle_atencion;
+    private int id_atencion;
+    private int id_medicamentos;
+    private String tratamiento;
+    private boolean estado;
+
+    public int getId_detalle_atencion() {
+        return id_detalle_atencion;
+    }
+
+    public void setId_detalle_atencion(int id_detalle_atencion) {
+        this.id_detalle_atencion = id_detalle_atencion;
+    }
+
+    public int getId_atencion() {
+        return id_atencion;
+    }
+
+    public void setId_atencion(int id_atencion) {
+        this.id_atencion = id_atencion;
+    }
+
+    public int getId_medicamentos() {
+        return id_medicamentos;
+    }
+
+    public void setId_medicamentos(int id_medicamentos) {
+        this.id_medicamentos = id_medicamentos;
+    }
+
+    public String getTratamiento() {
+        return tratamiento;
+    }
+
+    public void setTratamiento(String tratamiento) {
+        this.tratamiento = tratamiento;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
     
 }
--- Entidades/src/entidades/Medicamentos.java
+++ Entidades/src/entidades/Medicamentos.java
@@ -10,5 +10,87 @@ package entidades;
  * @author Alberto
  */
 public class Medicamentos {
+    private int id_medicamento;
+    private String descripcion; 
+    private String fechaElaboracion; 
+    private String fechaVencimiento;
+    private String marca;
+    private String categoria;
+    private String stock;
+    private boolean estado;
+    private String laboratorio;
+
+    public int getId_medicamento() {
+        return id_medicamento;
+    }
+
+    public void setId_medicamento(int id_medicamento) {
+        this.id_medicamento = id_medicamento;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getFechaElaboracion() {
+        return fechaElaboracion;
+    }
+
+    public void setFechaElaboracion(String fechaElaboracion) {
+        this.fechaElaboracion = fechaElaboracion;
+    }
+
+    public String getFechaVencimiento() {
+        return fechaVencimiento;
+    }
+
+    public void setFechaVencimiento(String fechaVencimiento) {
+        this.fechaVencimiento = fechaVencimiento;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getStock() {
+        return stock;
+    }
+
+    public void setStock(String stock) {
+        this.stock = stock;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public String getLaboratorio() {
+        return laboratorio;
+    }
+
+    public void setLaboratorio(String laboratorio) {
+        this.laboratorio = laboratorio;
+    }
+    
     
 }
