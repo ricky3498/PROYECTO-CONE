16c1f0538fa5cf43a0270e67e4b68ac1ec1d86bc
Alberto1004 <56856573+Alberto1004@users.noreply.github.com>
2019-11-23 19:11:03 -0500

    entidades02

---
+++ Entidades/src/entidades/Atencion_medica.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+/**
+ *
+ * @author Alberto
+ */
+public class Atencion_medica {
+    private int id_atencion;
+    private int id_cita;
+    private String fecha_atencion;
+    private boolean estado;
+    private String diagnostico;
+
+    public int getId_atencion() {
+        return id_atencion;
+    }
+
+    public void setId_atencion(int id_atencion) {
+        this.id_atencion = id_atencion;
+    }
+
+    public int getId_cita() {
+        return id_cita;
+    }
+
+    public void setId_cita(int id_cita) {
+        this.id_cita = id_cita;
+    }
+
+    public String getFecha_atencion() {
+        return fecha_atencion;
+    }
+
+    public void setFecha_atencion(String fecha_atencion) {
+        this.fecha_atencion = fecha_atencion;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public String getDiagnostico() {
+        return diagnostico;
+    }
+
+    public void setDiagnostico(String diagnostico) {
+        this.diagnostico = diagnostico;
+    }
+    
+}
